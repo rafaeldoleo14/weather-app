9a38dd2bc0233e96d46e9cc57d054c28c88788c2
Rafael Francisco <rafaeldeoleo14@gmail.com>
2023-03-24 18:21:29 -0400

    fix: dividing components

---
+++ src/weatherApp/components/Points/Points.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Points = () => {
+  return (
+    <>
+    
+        {/* Contenedor de los grados */}
+        <div style={{boxShadow: '3px 5px 1px gray'}}
+            className='d-flex justify-content-center align-items-center position-relative bg-light opacity-50 w-50 p-3 rounded-3'>
+
+            <h1 style={{textShadow: '2px 1px 1px gray', fontSize: '4em'}} className='text-black'>15°C</h1>
+            
+        </div>
+    
+    </>
+  )
+}
---
+++ src/weatherApp/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+export const SearchInput = () => {
+  return (
+    <>
+    
+        {/* Contenedor del input */}
+        <div className='d-flex justify-content-center position-absolute top-0 w-100 z-1'>
+            <input type="text" className='form-control w-75 rounded-0 rounded-bottom-4 focus'/>
+        </div>
+    
+    </>
+  )
+}
